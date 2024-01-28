2b5d0ee3d917611355473a1bdbb86a143769272f
crusaderky <crusaderky@gmail.com>
2024-01-28 00:18:08 +0000

    coverage

--- xarray_extras/numba_extras.py
+++ xarray_extras/numba_extras.py
@@ -7,6 +7,23 @@ from typing import Any
 
 import numba
 
+_DTYPES = (
+    # uint needs to appear before signed int:
+    # https://github.com/numba/numba/issues/2934
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "float32",
+    "float64",
+    "complex64",
+    "complex128",
+)
+
 
 def guvectorize(
     signature: str, layout: str, **kwargs: Any
@@ -39,24 +56,8 @@ def guvectorize(
        Discussing upstream fix; see
        `<https://github.com/numba/numba/issues/2936>`_.
     """
-    dtypes = [
-        # uint needs to appear before signed int:
-        # https://github.com/numba/numba/issues/2934
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-        "int8",
-        "int16",
-        "int32",
-        "int64",
-        "float32",
-        "float64",
-        "complex64",
-        "complex128",
-    ]
     if "{T}" in signature:
-        signatures = [signature.format(T=dtype) for dtype in dtypes]
+        signatures = [signature.format(T=dtype) for dtype in _DTYPES]
     else:
         signatures = [signature]
     kwargs.setdefault("cache", True)
